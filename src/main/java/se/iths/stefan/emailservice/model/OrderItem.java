6e9faae27708260dcf721619e0ba3e95e05813ed
stefanvill <Stefan@villegas.se>
2026-05-28 13:23:38 +0200

    Jag har lagt över kod från spring messenger och gjort så att susbcriber fungerar. listener tar emot och skickar sedan vidare via mail

---
+++ src/main/java/se/iths/stefan/emailservice/model/OrderItem.java
@@ -0,0 +1,15 @@
+package se.iths.stefan.emailservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class OrderItem {
+    private String name;
+    private double price;
+}
+
+
--- src/main/java/se/iths/stefan/emailservice/subscriber/MessageSubscriber.java
+++ src/main/java/se/iths/stefan/emailservice/subscriber/MessageSubscriber.java
@@ -4,20 +4,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import se.iths.stefan.emailservice.config.RabbitConfig;
+import se.iths.stefan.emailservice.model.Order;
 import se.iths.stefan.springmessenger.messaging.EmailSender;
-import se.iths.stefan.springmessenger.model.Message;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
 public class MessageSubscriber {
-    //
-    private final EmailSender sender;
 
+    private final EmailSender sender;
+    private final ObjectMapper objectMapper;
 
     @RabbitListener(queues = RabbitConfig.QUEUE)
-    public void subscribe(Message message) {
-        System.out.println("Recieved message: ");
-        sender.send(message);
+    public void subscribe(String message) {
+        try {
+            Order order = objectMapper.readValue(message, Order.class);
+
+            System.out.println("Received message: " + order);
+
+            String mail = "Hejsan " + order.getCustomerName()
+                    + ", tusen tack för din order. "
+                    + "Du har beställt " + order.getOrderItems()
+                    + ". Totalpris är: " + order.getTotalPrice();
+
+            sender.send(mail, order.getCustomerName());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse message", e);
+        }
     }
 }
-
