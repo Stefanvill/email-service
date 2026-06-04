3a3ec054165659e97816c642fcf72c58e33615db
stefanvill <Stefan@villegas.se>
2026-06-04 13:52:28 +0200

    redo för merge

--- src/main/java/se/iths/stefan/emailservice/subscriber/MessageSubscriber.java
+++ src/main/java/se/iths/stefan/emailservice/subscriber/MessageSubscriber.java
@@ -1,8 +1,6 @@
 package se.iths.stefan.emailservice.subscriber;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import se.iths.stefan.emailservice.config.RabbitConfig;
@@ -14,8 +12,6 @@ import tools.jackson.databind.ObjectMapper;
 @RequiredArgsConstructor
 public class MessageSubscriber {
 
-    private static final Logger log = LoggerFactory.getLogger(MessageSubscriber.class);
-
     private final ObjectMapper objectMapper;
     private final EmailSender sender;
 
@@ -24,17 +20,15 @@ public class MessageSubscriber {
         try {
             Order order = objectMapper.readValue(message, Order.class);
 
-            log.info("Received message: {}", order);
 
             String mail = "Hejsan " + order.getCustomerName()
                     + ", tusen tack för din order. "
                     + "Du har beställt " + order.getItems()
                     + ". Totalpris är: " + order.getTotalPrice();
 
-            log.info(mail);
             sender.send(mail, order.getCustomerName());
         } catch (Exception e) {
-            log.error("Failed to parse message", e);
+            System.out.println("Could not send message");
         }
     }
 }
