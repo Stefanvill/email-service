940ef70c17f1b5ca983d29d66ad3e57c1f2fcf8f
stefanvill <Stefan@villegas.se>
2026-06-04 13:36:49 +0200

    test merga ej

--- src/main/java/se/iths/stefan/emailservice/model/Order.java
+++ src/main/java/se/iths/stefan/emailservice/model/Order.java
@@ -3,15 +3,12 @@ package se.iths.stefan.emailservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Order {
-    private long id;
-    private LocalDateTime orderDate;
+
     private String customerName;
     private List<OrderItem> orderItems;
     private double totalPrice;
